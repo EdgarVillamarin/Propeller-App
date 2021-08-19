6f3aa601daf565c32bc43127d8bbfd6b3e243ba1
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 17:03:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-siteHeader = st.ontainer()
+siteHeader = st.container()
 dataExploration = st.container()
 newFeatures = st.container()
 modelTraining = st.container()
