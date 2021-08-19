c1c3a777b1da9a6ad5ca8a20e2aa6e8203dd9ba0
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 15:35:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import webbrowser
 siteHeader = st.beta_container()
 dataExploration = st.beta_container()
 newFeatures = st.beta_container()
@@ -10,3 +10,9 @@ with siteHeader:
   st.header('by [Nav.Eng. Edgar Villamarin](www.grupo-villamarin.com)')
   st.subheader('mail: e_villamarin@hotmail.com')
   st.text('In this project')
+
+
+url = 'www.grupo-villamarin.com'
+
+if st.button('Open browser'):
+    webbrowser.open_new_tab(url)
