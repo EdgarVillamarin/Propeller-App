881ed551ef2a0a0e4d8d98354107702dbca544be
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 14:57:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Ship Propeller!
+# Ship Propeller
 ### by Edgar Villamarin
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
