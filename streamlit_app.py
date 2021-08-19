d7ef89c0e85a223b146f56cc9101d9c7c5324631
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 15:22:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,12 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-import numpy as np
-"""
-# Ship Propeller
-### by Edgar Villamarin
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+siteHeader = st.beta_container()
+dataExploration = st.beta_container()
+newFeatures = st.beta_container()
+modelTraining = st.beta_container()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with siteHeader:
+  st.title('Welcome to Ship Propeller Project')
+  st.header('by Nav.Eng. Edgar Villamarin')
+  st.subheader('e_villamarin@hotmail.com')
+  st.text('In this project')
