3d66ed82a2e794bf6f3466f1e66d3b5f83a986be
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 20:32:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,8 @@ def wage(ja,PD,AEAO,z):
   KT, KQ and Efficiency Curves for the Wageningen B-Series Propellers
   http://deepblue.lib.umich.edu/handle/2027.42/3557
   """
-  mat = pd.read_csv('Coefficients/Wagenigen_coef.csv')
+  mat = pd.read_csv('Coefficients/Empuje_Wage.csv')
+  mat1=pd.read_csv('Coefficients/Torque_Wage.csv')
   KT = sum(mat['WagCThrust_stuv']*((ja)**mat['WagThrust_s'])*PD**mat['WagThrust_t']*AEAO**mat['WagThrust_u']*z**mat['WagThrust_v'])
   KQ= sum(mat['WagCTorque_stuv']*((ja)**mat['WagTorque_s'])*PD**mat['WagTorque_t']*AEAO**mat['WagTorque_u']*z**mat['WagTorque_v'])
   KT=float(KT)
