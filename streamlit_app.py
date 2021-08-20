b8446120e9ea26094860a9cd99e404ebe5ff80e8
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 23:23:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def get_table_download_link_csv(df,PD,AEAO,z):
     csv = df.to_csv().encode()
     #b64 = base64.b64encode(csv.encode()).decode() 
     b64 = base64.b64encode(csv).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="B serie"+"_P/D"+"_"str(PD)+"_AEAO"+"_"str(AEAO)+"_z"+"_"str(z)+".csv" target="_blank">Download csv file</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download='B serie'+'_P/D'+'_'str(PD)+'_AEAO'+'_'str(AEAO)+'_z'+'_'str(z)+'.csv' target="_blank">Download csv file</a>'
     return href
 
 def plot_propeller(Data,PD,AEAO,z):
