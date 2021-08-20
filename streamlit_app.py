257b3819934bcf977890b35a67aaaf7098d35e54
Edgar Gonzalo Villamarin Garcia <89162484+EdgarVillamarin@users.noreply.github.com>
2021-08-19 20:46:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,14 @@ newFeatures = st.container()
 modelTraining = st.container()
 
 def plot_propeller(Data,PD,AEAO,z):
+  import matplotlib.pyplot as plt
   head=list(Data.columns)
   fig,ax=plt.subplots(figsize=(7,3))
   ax.plot(Data['J'],Data['KT'],'k',Data['J'],10*Data['KQ'],'r',Data['J'],Data['no'])
   ax.set_xlabel('Advanced Coefficient   J')
   ax.set_ylabel('Kt,10*Kq,no')
   ax.set_title('B-serie'+'  '+'z:'+' '+str(z)+'  '+'AEAO:'+' '+str(AE/AO)+'  '+'PD:'+' '+str(P/D))
-  plot.grid()
+  plt.grid()
   return fig
   
 def wage(ja,PD,AEAO,z):
